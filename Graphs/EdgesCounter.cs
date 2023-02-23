79234d8bf7b26e26bb18121052e9bdfbd5c0942f
Vsevolod Vasilev <fdddfdfs@yandex.ru>
2023-02-24 02:16:02 +0400

    Count number of edges in graph

---
+++ Graphs/EdgesCounter.cs
@@ -0,0 +1,22 @@
+﻿namespace chapter_5;
+
+public class EdgesCounter
+{
+    public static int CountEdges(Graph g)
+    {
+        int count = 0;
+        
+        for (int i = 0; i < g.getVertices(); i++)
+        {
+            LinkedList.Node currentNode = g.getArray()[i].GetHead();
+            
+            while (currentNode != null)
+            {
+                count++;
+                currentNode = currentNode.nextElement;
+            }
+        }
+        
+        return count / 2;
+    }
+}
