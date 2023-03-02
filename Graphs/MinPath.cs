c20d799b231c4bd95d0152e5aed6222536c108da
Vsevolod Vasilev <fdddfdfs@yandex.ru>
2023-03-03 01:22:17 +0400

    Fixed loop

--- Graphs/MinPath.cs
+++ Graphs/MinPath.cs
@@ -6,11 +6,14 @@ public class MinPath
     {
         if (source == destination) return 0;
 
-        return FindPath(g, source, destination, 0);
+        bool[] visited = new bool[g.getVertices()];
+
+        return FindPath(g, source, destination, 0, visited);
     }
 
-    private static int FindPath(Graph g, int source, int destination, int depth)
+    private static int FindPath(Graph g, int source, int destination, int depth, bool[] visited)
     {
+        visited[source] = true;
         depth++;
         LinkedList.Node current = g.getArray()[source].GetHead();
 
@@ -21,10 +24,13 @@ public class MinPath
                 return depth;
             }
 
-            int result = FindPath(g, current.data, destination, depth);
+            if (!visited[current.data])
+            {
+                int result = FindPath(g, current.data, destination, depth, visited);
 
-            if (result != -1)
-                return result;
+                if (result != -1)
+                    return result;
+            }
 
             current = current.nextElement;
         }
